8108d2cdca38253ef4aa26324efe222ed8baa66a
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-09-07 00:42:26 +0100

    Models done

--- models/index.js
+++ models/index.js
@@ -2,3 +2,30 @@
 const User = require("./user");
 const Post = require("./post");
 const Comment = require("./comment");
+
+// Relationships:
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+module.exports = { User, Post, Comment };
