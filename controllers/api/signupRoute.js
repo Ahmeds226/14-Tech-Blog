69abb2e8f71930d1864c049f697a597ea518483b
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-09-07 01:57:28 +0100

    comlpeted all routes // begin contollers

---
+++ controllers/api/signupRoute.js
@@ -0,0 +1,34 @@
+// Imports:
+const router = require("express").Router();
+const { response } = require("express");
+const { User } = require("../../models");
+
+router.post("/", async (req, res) => {
+  const findUser = await User.findOne({
+    where: { user_name: req.body.user_name },
+  });
+
+  if (findUser) {
+    res.status(400).json({
+      message:
+        "Seems as though there is a user with that username. Click the login button to sign in or please pick an alternate username",
+    });
+    return;
+  }
+  try {
+    const user = await User.create({
+      user_name: req.body.user_name,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.user_id = user.id;
+      req.session.logged_in = true;
+      res.status(200).json(user);
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
